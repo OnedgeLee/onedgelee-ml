6e253925e0ee9d748a6a908759de7f75f5c8e985
Onedge <Onedge.Lee@gmail.com>
2018-06-16 19:41:29 +0900

    20180616

--- examples/4_multinomial_classification/fancy_softmax_classification.py
+++ examples/4_multinomial_classification/fancy_softmax_classification.py
@@ -6,7 +6,7 @@ x_data = xy[:, 0:-1]
 y_data = xy[:, [-1]]
 
 X = tf.placeholder(tf.float32, shape=[None, 16])
-Y = tf.placeholder(tf.float32, shape=[None, 1])
+Y = tf.placeholder(tf.int32, shape=[None, 1])
 
 nb_classes = 7
 Y_one_hot = tf.one_hot(Y, nb_classes)
@@ -19,9 +19,27 @@ b = tf.Variable(tf.random_normal([nb_classes]), name='bias')
 
 logits = tf.matmul(X, W) + b
 hypothesis = tf.nn.softmax(logits)
-cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y_one_hot)
+cost_i = tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y_one_hot)
 cost = tf.reduce_mean(cost_i)
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 train = optimizer.minimize(cost)
 
+prediction = tf.argmax(hypothesis, 1)
+correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(2001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
+        if step % 200 == 0:
+            acc_val = sess.run(accuracy, feed_dict={X: x_data, Y: y_data})
+            print(step, cost_val, acc_val)
+
+    pred = sess.run(prediction, feed_dict={X: x_data, Y: y_data})
+
+# zip, flatten 찾아볼 것
+    for p, y in zip(pred, y_data.flatten()):
+        print('[{}] prediction: {} True Y: {}'.format(p == int(y), p, int(y)))
--- examples/4_multinomial_classification/softmax_classification.py
+++ examples/4_multinomial_classification/softmax_classification.py
@@ -31,3 +31,33 @@ with tf.Session() as sess:
     h = sess.run(hypothesis, feed_dict={X: x_data})
     print(h, sess.run(tf.argmax(h, 1)))
 # tf.argmax의 두 번째 parameter은 one-hot-encoding을 적용할 차원
+
+""" 
+a3 = tf.Variable([[[0.1, 0.3, 0.5],
+                   [0.3, 0.5, 0.1]],
+                  [[0.5, 0.1, 0.3],
+                   [0.1, 0.3, 0.5]],
+                  [[0.3, 0.5, 0.1],
+                   [0.5, 0.1, 0.3]]])
+
+functions.showOperation(tf.argmax(a3, 0))
+functions.showOperation(tf.argmax(a3, 1))
+functions.showOperation(tf.argmax(a3, 2))
+
+[출력 결과]
+[[1 2 0]
+ [2 0 1]]
+
+[[1 1 0]
+ [0 1 1]
+ [1 0 1]]
+
+[[2 1]
+ [0 2]
+ [1 0]] 
+
+간단히, 0차원 진입, 1차원 진입, 2차원 진입 기준으로 동일 차원 기준으로 비교한다고 생각하면 됨
+0: 0번 진입하여 최외각 원소들 3개 띄엄띄엄 비교
+1: 1번 진입하여 2개 원소 비교
+2: 끝까지 진입하여 3개 원소 비교
+"""
